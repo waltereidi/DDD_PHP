582798c2ccf7c5f64fcb04d376737c664ebea1bc
root <root@PROPONTO>
2024-12-30 11:47:44 -0300

    modificado start.sh para adicionar ao hosts o IP do docker internal

--- docker/docker/start.sh
+++ docker/docker/start.sh
@@ -11,7 +11,7 @@ ln -s /etc/nginx/sites-available/front.conf /etc/nginx/sites-enabled/
 
 curl -1sLf 'https://dl.cloudsmith.io/public/symfony/stable/setup.deb.sh' | sudo -E bash
 sudo apt install symfony-cli
-
+echo "127.0.0.1 host.docker.internal" >> /etc/hosts
 sudo service nginx start
 
 sudo tail -f /dev/null
